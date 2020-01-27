963b790c5090aa07aa4355af06965ddd3f0b3a34
Ominous48 <andrea2@mail.ru>
2020-01-27 14:48:27 +0300

    fifth commit

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,15 +1,28 @@
 import os
+import pytest
 
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    os.environ['MOLECULE_INVENTORY_FILE']
-).get_hosts('all')
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+@pytest.mark.parametrize('pkg', [
+  'chrony'
+])
 
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+def test_chrony_installed(host, pkg):
+    package = host.package(pkg)
+    assert package.is_installed
+
+def test_chrony_service_started_enabled(host):
+    chrony_service_name = _get_chrony_package_name(host.system_info.distribution)
+    chrony_service = host.service(chrony_service_name)
+    assert chrony_service.is_running
+    assert chrony_service.is_enabled
+
+def _get_chrony_package_name(host_distro):
+    return {
+        "debian": "chrony",
+        "centos": "chronyd"
+    }.get(host_distro, "chrony")
