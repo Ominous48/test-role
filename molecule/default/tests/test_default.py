b51f9205e0915d5542bcf3f5ae869005dd602158
Ominous48 <andrea2@mail.ru>
2020-01-27 14:57:57 +0300

    fifth commit3

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -10,16 +10,18 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 @pytest.mark.parametrize('pkg', [
   'chrony'
 ])
-
 def test_chrony_installed(host, pkg):
     package = host.package(pkg)
     assert package.is_installed
 
+
 def test_chrony_service_started_enabled(host):
-    chrony_service_name = _get_chrony_package_name(host.system_info.distribution)
+    chrony_service_name = _get_chrony_package_name(
+            host.system_info.distribution)
     chrony_service = host.service(chrony_service_name)
     assert chrony_service.is_running
     assert chrony_service.is_enabled
+
 
 def _get_chrony_package_name(host_distro):
     return {
